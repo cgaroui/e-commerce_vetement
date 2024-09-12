f105b969d84fbd5fe17e48907b80a6eaa36407c6
chaima garoui <chaimagaroui2@gmail.com>
2024-09-12 23:46:56 +0200

    ajout : systeme de  pagination ->affichage seulment de 9 produit par page

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\ProduitRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,10 +12,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class ProduitController extends AbstractController
 {
     #[Route('/produit', name: 'app_produit')]
-    public function index(ProduitRepository $repository, Request $request): Response
+    public function index(ProduitRepository $repository, Request $request, PaginatorInterface $paginator): Response
     {
-        $produits = $repository->findAll();
-            // $request->
+        $produits = $paginator->paginate( 
+        $repository->findAll(),
+        $request->query->getInt('page', 1), /*page number*/
+        9 /*limit par page*/
+            );
 
         return $this->render('produit/index.html.twig', [
             'produits' => $produits
