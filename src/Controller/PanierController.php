9dec101f5cfc8ae0ccaf102c6d769e017a25874e
chaima garoui <chaimagaroui2@gmail.com>
2024-10-15 13:15:04 +0200

    ajout produit au panier fonctionnel

--- src/Controller/PanierController.php
+++ src/Controller/PanierController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Produit;
+use App\Repository\ProduitRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
@@ -11,16 +12,57 @@ use Symfony\Component\Routing\Attribute\Route;
 class PanierController extends AbstractController
 {
     #[Route('/panier', name: 'app_panier')]
-    public function index(): Response
+    public function index(SessionInterface $session, ProduitRepository $ProduitRepository)
     {
-        return $this->render('panier/index.html.twig', [
-            'controller_name' => 'PanierController',
+        $panier = $session->get('panier', []);
+
+        //initialisation des variables 
+        $data = [];
+        $total = 0; 
+
+       
+        // $session->set('panier',[]); // pour supprimer le panier car j'avais un produit inexistant 
+
+        //on parcourt le paniser sous forme clé valeur de chque produit par son id et sa quantité 
+        foreach($panier as $id => $quantite){
+            $produit = $ProduitRepository->find($id);
+            $data[] = [
+                'produit' =>$produit,
+                'quantite' => $quantite
+            ];
+            $total+= $produit->getPrix() * $quantite;
+            
+        }
+            // dd($totalCommande);
+        return $this->render('panier/index.html.twig',
+         [
+            'produit' => $produit,
+            'quantite' => $quantite,
+            'data' => $data
         ]);
     }
 
     #[Route('/panier/ajout/{id}', name: 'ajout_panier')]
     public function ajoutAuPanier(Produit $produit,SessionInterface $session )
     {
-        dd($session);
+        //on recupere l'id du produit 
+        $id = $produit->getId();
+
+        //on recupere le panier existant , s'il existe pas dans ma session je met le tableau vide d'ou les []
+        $panier = $session->get('panier', []);
+
+        //on ajoute le produit dans le panier s'il n'yest pas encore 
+        //sinon j'incrémente la quantitée
+        if(empty($panier[$id])){
+             $panier[$id] = 1;
+        }else{
+            $panier[$id]++;
+        }
+        $session->set('panier', $panier);
+      //on redirige vers lepanier 
+      return $this->redirectToRoute('app_panier');
+
+    
+     
     }
 }
